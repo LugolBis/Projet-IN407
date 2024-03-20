434a435d7d51aed464ffbf622e4e4c1e6dfa40c1
Loïc <147602359+LugolBis@users.noreply.github.com>
2024-03-20 14:23:31 +0100

    Changements d'architecture
    Les changements sont les suivants : 
    - Bug d'héritage de la classe Buffer pour les objets de la classe Source réglé.
    - Changements de paramètres pour effectuer des tests (capacité locale du Buffer "Destination" dans la classe Stratégie, débit du Buffer principal concrètement immédiat, réduction de l'intervalle d'attente entre la génération de deux paquets).
    - Amélioration de l'implémentation de la classe Stratégie (pour l'heure encore incomplète)
    - Ajout des méthodes ".Update()" et ".Update_Transmission()" pour remplacer la notion de threads ?? A vérifier la notion de parallélisme dans l'attente des objets.

--- Projet IN407.py
+++ Projet IN407.py
@@ -7,13 +7,17 @@ class Paquet:
         self.valeur = Paquet.nombre_paquets+1
         self.source = source
 
+    def __repr__(self):
+        return str(self.valeur)
+
 class Buffer:
     nombre_buffers = 0
     Capacité = 100 
-    def __init__(self,predecesseur=[],successeur=None):
+    def __init__(self,successeur=None):
         # L'idée derrière prédescesseur et successeur est de lier les buffer entre eux, mais aussi à leur Source notamment pour simplifier la transmission des paquets
         # Remarque : un buffer peut avoir plusieurs prédescesseurs 
         Buffer.nombre_buffers += 1
+        predecesseur=[]
         self.predecesseur = predecesseur
         self.successeur = successeur
         liste_attente = []
@@ -32,10 +36,13 @@ class Buffer:
             self.successeur.Insertion(paquet=self.liste_attente.pop(0)) 
             self.capacite_locale -= 1     
 
-class Source:
+    def Update_Transmission(self):
+        self.Transmission()
+
+class Source(Buffer):
     nombre_sources = 0
     def __init__(self,Successeur_2:None): ####### Successeur_2 serait le buffer principal
-        Buffer.__init__(self,successeur=Successeur_2)  # Héritage -> chaque source à son propre Buffer intégré
+        super().__init__(Successeur_2)  # Héritage -> chaque source à son propre Buffer intégré
         Source.nombre_sources += 1
         numéro = Source.nombre_sources # On initialise le numéro de la source
         self.numéro = numéro
@@ -43,43 +50,55 @@ class Source:
         self.active = active
         fournit = []  # sauvegarde des paquets générés
         self.fournit = fournit
+        temps = 0
+        self.temps = temps
 
     def Generateur_paquet(self):
         # Générateur de paquet !!!! Attention il ne respecte pas encore la loi de poisson ---- a vérifier 
         from random import randint
         import time
-        temps = 0
-        while self.active == True:
-            temps_delta = randint(1,10)
-            temps += temps_delta
-            time.sleep(temps_delta)  ##################### cela implique d'utiliser des threads lors de l'éxécution de la fonction afin de ne pas arrêter tous le script 
-            paquet = Paquet(source=self.numéro)
-            self.fournit.append(paquet)
-            self.Insertion(paquet)   
+        temps_delta = randint(1,5)
+        self.temps += temps_delta
+        time.sleep(temps_delta)  ##################### cela implique d'utiliser des threads lors de l'éxécution de la fonction afin de ne pas arrêter tous le script 
+        paquet = Paquet(source=self.numéro)
+        print(f"paquet n°{paquet} provenant de {self.numéro} - temps : {self.temps}")
+        self.fournit.append(paquet)
+        self.Insertion(paquet)   
                                              
-    def Stop_generateur_paquet(self):
-        self.active = False # C'est une première idée certainement maladroite et inadaptée aux threads, il serait sûrement plus simple de mettre fin au thread dans lequel 'Generateur_paquet()' à été lancé
-                            # Ou il faudrait que cette fonction soit lancée dans le même ""espace mémoire"" que le thread de la fonction 'Generateur_paquet()' --> ou utiliser la communication entre les processus
-        
+    def Update(self):
+        if self.active == True:
+            self.Generateur_paquet()
+                           
 class Stratégie:
     # Cette classe à pour but d'encapsuler les stratégies de flux de données et d'encapsuler les quelques variables nécessaires au déroulement du script (a priori environ 5)
     def __init__(self,numéro):
         assert numéro in [1,2,3], "Il n'existe que 3 stratégie différente."
-        Destination = Buffer() ; Destination.capacite_locale = -1000 # On aggrandit la capacité locale de Destination en l'initialisant à -1000 au lieu de 0 pour les autres objets
+        Destination = Buffer() ; Destination.capacite_locale = 80 # On aggrandit la capacité locale de Destination en l'initialisant à -1000 au lieu de 0 pour les autres objets
         self.Destination = Destination
         if numéro == 1:
             # Implémentation de la stratégie n°1
-            Buffer_Principal = Buffer()
+            Buffer_Principal = Buffer(Destination)
             SourceA = Source(Successeur_2=Buffer_Principal)
             SourceB = Source(Successeur_2=Buffer_Principal)
             Buffer_Principal.predecesseur = [SourceA,SourceB]
             ############### Utilisation de Thread pour lancer l'éxécution de la génération de paquet, un tube de communication pourrait aussi être nécessaire.
             # Pour l'instant j'essaie de mettre en place une implémentation simple, sans parallélisme de procéssus pour tester préalablement les classes.
-            SourceA.Generateur_paquet()
-            SourceB.Generateur_paquet()
+            SourceA.Generateur_paquet() ; SourceB.Generateur_paquet()
+            test = 0
             while Destination.capacite_locale < Buffer.Capacité:
+                test += 1
+                SourceA.Update() ; SourceB.Update()
+                print(f"Buffer Principal : {Buffer_Principal.liste_attente}")
+                print(f"Buffer Destination : {Destination.liste_attente}")
                 if SourceA.capacite_locale > SourceB.capacite_locale:
                     SourceA.Transmission()
                 else:
                     SourceB.Transmission()
+
+                if test > 5:
+                    Buffer_Principal.Update_Transmission() ; Destination.Update_Transmission()
+
             SourceA.active = False ; SourceB.active = False
+            print(SourceA.liste_attente)
+
+Test = Stratégie(1)
