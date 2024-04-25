e917ce550fed7d47a39fcb5dedb68f8a5b862328
Loïc <147602359+LugolBis@users.noreply.github.com>
2024-04-25 22:30:38 +0200

    Mécanisme d'installation - update

--- Projet_IN407_DD_Mecanisme_install.py
+++ Projet_IN407_DD_Mecanisme_install.py
@@ -1,22 +1,21 @@
 # Mécanisme d'installation du Projet IN407
 
-from setuptools import setup, find_packages
+import subprocess
+import sys
 
-setup(
-    name='Projet_IN407_DD',
-    version='1.0',
-    packages=find_packages(),
-    scripts=['Projet_IN407_Desmares_Desfontaines.py'],
-    install_requires=[
-        'time', 'random', 'threading', 'os', 'platform', 'subprocess'
-    ],
-    entry_points={
-        'console_scripts': [
-            'nom_de_commande = Projet_IN407_Desmares_Desfontaines:fonction_principale',
-        ]
-    },
-    author='Loïc Desmares - Alexia Desfontaines',
-    author_email='',
-    description="Projet_IN407_DD est une application permettant de comparer des stratégies de gestion de flux de données au sein d'un réseau.",
-    url='https://github.com/LugolBis/Projet-IN407/tree/main',
-)
+# On initialise la liste des modules à installer 
+liste_modules = ['customtkinter']
+
+def install_modules(liste):
+    for module in liste:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", module])
+
+# Installation des modules nécessaires
+if __name__ == '__main__':
+    install_modules(liste_modules)
+
+# Les modules 'time', 'random' et 'threading' font partis de la bibliothèque standard de python 
+# Leur version est donc dépendante de l'interpréteur Python 
+
+version_python = sys.version.split()[0] # On récupère la version de l'interpréteur python 
+assert version_python >= '3.10.9', "Votre interpréteur python étant une ancienne version peut provoquer des erreurs avec l'application."
